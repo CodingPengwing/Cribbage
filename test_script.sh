c8699345bbe64a256a3bfdab91e2abc4877d9426
Tony Dang <htdang@student.unimelb.edu.au>
2021-05-29 16:59:32 +1000

    added testing scripts, run each test in cribbage.properties file then run ./test_script.sh in terminal to test

---
+++ test_script.sh
@@ -0,0 +1,6 @@
+diff expected-outputs/expected-cribbage-55510.log given-outputs/cribbage-55510.log
+diff expected-outputs/expected-cribbage-12345.log given-outputs/cribbage-12345.log
+diff expected-outputs/expected-cribbage-1248.log given-outputs/cribbage-1248.log
+diff expected-outputs/expected-cribbage-30006.log given-outputs/cribbage-30006.log
+diff expected-outputs/expected-cribbage-1793122953.log given-outputs/cribbage-1793122953.log
+diff expected-outputs/expected-cribbage-547024720.log given-outputs/cribbage-547024720.log
